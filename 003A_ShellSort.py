55e31edd720feea74e9ede55b040150e15f2f6ef
lixinchn <lixinit@gmail.com>
2016-05-12 16:05:28 +0800

    advanced shell sort

---
+++ 003A_ShellSort.py
@@ -0,0 +1,25 @@
+# http://blog.csdn.net/morewindows/article/details/6668714
+
+class ShellSort():
+    def __init__(self):
+        self.data = [10, 14, 73, 25, 23, 13, 27, 94, 33, 39, 25, 59, 94, 65, 82, 45]
+
+    def do(self):
+        data_len = len(self.data)
+        gap = data_len / 2
+        while gap > 0:
+            for i in range(gap, data_len):
+                j = i - gap
+                if self.data[i] < self.data[j]:
+                    self.data[i], self.data[j] = self.data[j], self.data[i]
+                    k = j - gap
+                    while k > 0:
+                        if self.data[k] > self.data[j]:
+                            self.data[k], self.data[j] = self.data[j], self.data[k]
+                        k -= gap
+            gap /= 2
+
+if __name__ == '__main__':
+    ss = ShellSort()
+    ss.do()
+    print ss.data
